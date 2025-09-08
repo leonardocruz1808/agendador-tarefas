6975e549028519fb3818efd32920780dda1e5812
Leonardo <l.a.cruz1808@gmail.com>
2025-09-08 20:45:47 -0300

    commit inicial com esqueleto

---
+++ src/main/java/com/leonardo/agendadorDeTarefas/AgendadorDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.leonardo.agendadorDeTarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorDeTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardo/agendadorDeTarefas/AgendadorDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.agendadorDeTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
