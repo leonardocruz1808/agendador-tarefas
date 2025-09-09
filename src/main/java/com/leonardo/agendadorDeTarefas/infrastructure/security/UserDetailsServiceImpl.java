165c3ceaf4cdca7fb167e80bddfeee0b7a484c9c
Leonardo <l.a.cruz1808@gmail.com>
2025-09-08 21:45:56 -0300

    adicionadas configurações de segurança

---
+++ src/main/java/com/leonardo/agendadorDeTarefas/infrastructure/security/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.leonardo.agendadorDeTarefas.infrastructure.security;
+
+
+
+
+import com.leonardo.agendadorDeTarefas.infrastructure.business.dto.UsuarioDTO;
+import com.leonardo.agendadorDeTarefas.infrastructure.client.UsuarioClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import static io.jsonwebtoken.security.Keys.password;
+
+@Service
+public class UserDetailsServiceImpl  {
+    @Autowired
+    private UsuarioClient client;
+
+
+    public UserDetails carregaDadosUsuario (String email, String token){
+
+        UsuarioDTO usuarioDTO = client.buscarUsuarioPorEmail(email, token);
+        return User
+                .withUsername(usuarioDTO.getEmail()) // Define o nome de usuário como o e-mail
+                .password(usuarioDTO.getSenha()) // Define a senha do usuário
+                .build();
+
+    }
+}
